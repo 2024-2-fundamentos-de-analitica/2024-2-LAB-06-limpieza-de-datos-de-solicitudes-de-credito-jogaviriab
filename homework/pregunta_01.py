44a693a0e73dcb56c3f58c934b5137568403cc6a
Jorge Gaviria <jogaviriab@unal.edu.co>
2025-02-09 15:04:25 -0500

    laboratorio listo

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -13,3 +13,40 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+
+    import re 
+    from pathlib import Path
+    import pandas as pd
+    import os
+
+    input_dir = Path("files/input/solicitudes_de_credito.csv")
+
+    df = pd.read_csv(input_dir , delimiter=";")
+
+    if "Unnamed: 0" in df.columns:
+        df.drop(columns=["Unnamed: 0"],inplace=True)
+
+
+    df["sexo"] = df["sexo"].str.lower()
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower().str.strip()
+    df["idea_negocio"] = df["idea_negocio"].str.lower().str.replace("_", " ").str.replace("-", " ").str.strip()
+    df["barrio"] = df["barrio"].str.lower().str.replace("_", " ").str.replace("-", " ")
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+    df["monto_del_credito"] = df["monto_del_credito"].str.strip().str.replace("$", "").str.replace(",", "").str.replace(".00", "").astype(int)
+    df["línea_credito"] = df["línea_credito"].str.lower().str.strip().str.replace("_", " ").str.replace("-", " ").str.strip()
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+    df["fecha_de_beneficio"] = pd.to_datetime(
+        df["fecha_de_beneficio"], format="%d/%m/%Y", errors="coerce"
+    ).combine_first(
+        pd.to_datetime(df["fecha_de_beneficio"], format="%Y/%m/%d", errors="coerce")
+    )
+
+    df = df.drop_duplicates()
+    df = df.dropna()
+    
+
+    if not os.path.exists("files/output"):
+        os.makedirs("files/output")
+    df.to_csv("files/output/solicitudes_de_credito.csv", sep=';', index=False)
+    
+pregunta_01()
